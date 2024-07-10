74e58ceb3c32400be283bea0eeb5f5e83cd823e7
AliZokaeai <alizokaeaidehaghani@gmail.com>
2024-07-10 21:55:29 +0330

    learn Intersting thing about PHP

--- PHP-learning/Php/first-php-file.php
+++ PHP-learning/Php/first-php-file.php
@@ -5,12 +5,50 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+    .body-1 {
+        background-color: red;
+        color:aliceblue;
+        font-size: <?php echo rand(10,30);?>px;
+    }
+
+    .body-2 {
+        background-color: green;
+        color:yellow;
+        font-size: <?php echo rand(10,30);?>px;
+    }
+
+    .body-3 {
+        background-color: aqua;
+        color:white;
+        font-size: <?php echo rand(10,30);?>px;
+    }
+
+    .body-4 {
+        background-color: yellow;
+        color:red;
+        font-size: <?php echo rand(10,30);?>px;
+    }
+
+    .body-5 {
+        background-color: magenta;
+        color:aliceblue;
+        font-size: <?php echo rand(10,30);?>px;
+    }
+
+    .body-6 {
+        background-color: gray;
+        color:whitesmoke;
+        font-size: <?php echo rand(10,30);?>px;
+    }
+    </style>
 </head>
 
-<body>
-    <?php 
-    echo "<h1>Hello world</h1>"
+<body class="body-<?php echo rand(1,6);?>">
+    <?php
+        echo "<h1>Hello from PHP!</h1>";
     ?>
+
 
 </body>
 
